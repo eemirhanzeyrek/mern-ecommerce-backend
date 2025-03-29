bc79deef58e0ba3ea97079724b21dec2bd25c38b
eemirhanzeyrek <contactemirhanzeyrek@gmail.com>
2025-03-29 14:32:49 +0300

    first commit

---
+++ routes/orderRoute.js
@@ -0,0 +1,33 @@
+import express from "express";
+import adminAuth from "../middleware/adminAuth.js";
+import authUser from "../middleware/auth.js";
+import {
+  placeOrder,
+  placeOrderStripe,
+  placeOrderRazorpay,
+  allOrders,
+  userOrders,
+  updateStatus,
+  verifyStripe,
+  verifyRazorpay,
+} from "../controllers/orderController.js";
+
+const orderRouter = express.Router();
+
+//admin features
+orderRouter.post("/list", adminAuth, allOrders);
+orderRouter.post("/status", adminAuth, updateStatus);
+
+//payment features
+orderRouter.post("/place", authUser, placeOrder);
+orderRouter.post("/stripe", authUser, placeOrderStripe);
+orderRouter.post("/razorpay", authUser, placeOrderRazorpay);
+
+//user feature
+orderRouter.post("/userorders", authUser, userOrders);
+
+//verify payment
+orderRouter.post("/verifyStripe", authUser, verifyStripe);
+orderRouter.post("/verifyRazorpay", authUser, verifyRazorpay);
+
+export default orderRouter;
---
+++ server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
+import productRouter from "./routes/productRoute.js";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
+import "dotenv/config";
+
+//app config
+const app = express();
+const port = process.env.PORT || 4000;
+
+connectDB();
+connectCloudinary();
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+//api endpoints - routes
+app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
+
+app.get("/", (req, res) => {
+  res.send("api is running");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
